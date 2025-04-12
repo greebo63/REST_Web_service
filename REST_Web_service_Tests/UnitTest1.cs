0266051805c69ce669eaf22a3b073b16442ecb27
Naeel <nailkhakimov123@mail.ru>
2025-04-12 14:37:59 +0500

    Добавил юнит тест для случая, когда не был отправлен файл

--- REST_Web_service_Tests/UnitTest1.cs
+++ REST_Web_service_Tests/UnitTest1.cs
@@ -1,11 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using REST_Web_service.Controllers;
+
 namespace REST_Web_service_Tests
 {
-    public class UnitTest1
+    public class AdPlatformsControllerTests
     {
         [Fact]
-        public void Test1()
+        public async Task adPlatformsRefresh_noFile_badRequest()
         {
+            //Arrange
+            var controller = new testController();
 
+            //Act
+            var res = await controller.adPlatformsRefresh(null);
+
+            //Assert
+            BadRequestObjectResult badRequestResult = Assert.IsType<BadRequestObjectResult>(res);
+            Assert.Equal("Вы не загрузили файл", badRequestResult.Value);
         }
     }
 }
